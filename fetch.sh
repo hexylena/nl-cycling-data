84c0f4cf489f1fa07e912daf1136b070c099fa2b
Helena Rasche <hexylena@galaxians.org>
2024-03-24 16:12:38 +0100

    renames

--- fetch.sh
+++ fetch.sh
@@ -1,8 +1,8 @@
 # DH
-wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area(id:3600192736)->.searchArea; nwr["network"="rcn"](area.searchArea); out geom;" -O dh.geojson
-wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area(id:3600192736)->.searchArea; nwr["rcn_ref"](area.searchArea); out geom;" -O dh.nodes.geojson
+wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area(id:3600192736)->.searchArea; nwr["network"="rcn"](area.searchArea); out geom;" -O dh.turbojson
+wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area(id:3600192736)->.searchArea; nwr["rcn_ref"](area.searchArea); out geom;" -O dh.nodes.turbojson
 # NL
-wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area(id:3602323309)->.searchArea; nwr["network"="rcn"](area.searchArea); out geom;" -O holland.geojson
-rm -f holland.geojson.gz
-gzip holland.geojson
-wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area(id:3602323309)->.searchArea; nwr["rcn_ref"](area.searchArea); out geom;" -O holland.nodes.geojson
+wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area(id:3602323309)->.searchArea; nwr["network"="rcn"](area.searchArea); out geom;" -O holland.turbojson
+rm -f holland.turbojson.gz
+gzip holland.turbojson
+wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area(id:3602323309)->.searchArea; nwr["rcn_ref"](area.searchArea); out geom;" -O holland.nodes.turbojson
