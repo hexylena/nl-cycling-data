50eea18cb70a3b7b70f33cb62b6fc77d78046ca7
Helena Rasche <hexylena@galaxians.org>
2024-03-24 16:06:48 +0100

    gzip

--- fetch.sh
+++ fetch.sh
@@ -3,4 +3,6 @@ wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area
 wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area(id:3600192736)->.searchArea; nwr["rcn_ref"](area.searchArea); out geom;" -O dh.nodes.geojson
 # NL
 wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area(id:3602323309)->.searchArea; nwr["network"="rcn"](area.searchArea); out geom;" -O holland.geojson
+rm -f holland.geojson.gz
+gzip holland.geojson
 wget "https://overpass-api.de/api/interpreter?data=[out:json][timeout:325]; area(id:3602323309)->.searchArea; nwr["rcn_ref"](area.searchArea); out geom;" -O holland.nodes.geojson
